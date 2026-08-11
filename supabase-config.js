1bbc3b28ad71d5f05eb919e00f3017cfd5b9f1bd
torokpeter7 <torokpeter@turr.hu>
2026-08-11 21:21:24 +0200

    Vélemények beiktatása

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@
 // Töltsd ki a saját projekted adataival:
 // Supabase Dashboard → Project Settings → API
 // ---------------------------------------------------------
-const SUPABASE_URL = "https://vdoqjgpproivhyxrbcoa.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkb3FqZ3Bwcm9pdmh5eHJiY29hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTgxNDYsImV4cCI6MjEwMTY5NDE0Nn0.WajHxqgRrcgbBBqCmRQ2Bl5opi4wNK17rlblySXUjTY";
+const SUPABASE_URL = "https://supabase.netkapocs.torokhomelab.cloud";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg2MTQ2MzE5LCJleHAiOjE5NDM4MjYzMTl9.ZFWTXIv__Ptu-ajCx-Vql0RVfeJWQ-9sdwDBhgMuwEY";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
